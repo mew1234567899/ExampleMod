84ed017c91a29ccc7b577369ebc9af80a26f3b5d
mew1234567899 <iamaperson123thetwelve@gmail.com>
2021-02-20 23:01:57 -0600

    Update main.js
    had to fix this

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 //this should export a map into sector 174//
-const mapVariable = extend(SectorPreset, "mewsmap", Serpulo, 174, {
+const mapVariable = extend(SectorPreset, "mewsmap", Planets.serpulo, 174, {
     localizedName: "sadland",
-    description: "youll never laugh again",
+    description: "you'll never laugh again",
     difficulty: 2
 })
