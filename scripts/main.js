b025592f4aaa84c45e34833aaac4cccad118cd38
mew1234567899 <iamaperson123thetwelve@gmail.com>
2021-02-20 22:54:05 -0600

    Create main.js
    all i did was put a cheat map in here.

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+//this should export a map into sector 174//
+const mapVariable = extend(SectorPreset, "mewsmap", Serpulo, 174, {
+    localizedName: "sadland",
+    description: "youll never laugh again",
+    difficulty: 2
+})
